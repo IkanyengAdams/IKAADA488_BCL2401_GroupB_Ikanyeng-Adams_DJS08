a71ba80676c910a87f04a0e553280fa3c5f528be
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-06-07 16:41:31 +0200

    "Added HostVans component, updated App.jsx to include HostVans route, and modified server.js to include seeds and routes for vans"

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,7 @@ export default function App() {
                 <Route index element={<Dashboard />} />
                 <Route path="income" element={<Income />} />
                 <Route path="reviews" element={<Reviews />} />
+                <Route path="vans" element={<HostVans />} />
                 <Route path="vans" element={<HostVanDetail />} />
               </Route>
             </Route>
