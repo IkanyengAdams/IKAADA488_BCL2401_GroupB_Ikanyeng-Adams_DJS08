d373e6a964dbf1bbfc76f85fd796086444db657c
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-06-07 10:56:20 +0200

    "Updated App.jsx: renamed VansDetail to VanDetail, added new imports and routes for host features, and restructured navigation"

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Vans from "./Vans";
-import VansDetail from "./VanDetail";
+import VanDetail from "./VanDetail";
+import Dashboard from "../src/Host/Dashboard";
+import Income from "../src/Host/Income";
+import Reviews from "../src/Host/Reviews";
+import Layout from "../src/components/Layout";
 
 import "./server"
 
@@ -12,20 +16,17 @@ export default function App() {
   return (
     <BrowserRouter>
     <div className="site-wrapper">
-      <header>
-      <Link className="site-logo"  to="/home">#VANLIFE</Link>
-      <nav>
-        <Link  to="/about">About</Link>
-        <Link  to="/vans">Vans</Link>
-      </nav>
-      </header>
-
-    <main>
+      <main>
       <Routes>
+        <Route element={<Layout />}>
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VansDetail />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route path="/host" element={<Dashboard />} />
+        <Route path="/host/income" element={<Income />} />
+        <Route path="/host/reviews" element={<Reviews />} />
+        </Route>
       </Routes>
       </main>
 
