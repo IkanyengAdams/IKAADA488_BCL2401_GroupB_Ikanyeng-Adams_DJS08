fdbd6d939b945cba3b063f25a9713d77cfcc84a6
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-06-09 17:58:15 +0200

    Updated multiple components in src directory, including About, Home, Host/Dashboard, Host/HostVanDetail, Host/HostVans, Host/Income, NotFound, api.js, components/Header, and components/HostLayout.

--- src/About.jsx
+++ src/About.jsx
@@ -1,20 +1,27 @@
-import React from "react"
-import bgImg from "../src/assets/about-hero.png"
-import { Link } from "react-router-dom"
-
-
+import React from "react";
+import bgImg from "../src/assets/about-hero.png";
+import { Link } from "react-router-dom";
 
 export default function About() {
   return (
-    
     <div className="about-page-content">
       <img src={bgImg} alt="About VanLife" className="about-hero-image" />
-  <h1>Don't squeeze in a sedan when you could relax in a van.</h1>
-  <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch. (Hitch costs extra)<br></br>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
-  <div className="about-page-cta">
-    <h2>Your destination is waiting. Your van is ready.</h2>
-    <Link to="/vans" className="link-button">Explore our vans</Link>
-  </div>
-  </div>
-  )
+      <div className="about-page-content">
+      <h1>Don't squeeze in a sedan when you could relax in a van.</h1>
+      <p>
+        Our mission is to enliven your road trip with the perfect travel van
+        rental. Our vans are recertified before each trip to ensure your travel
+        plans can go off without a hitch. (Hitch costs extra)<br></br>Our team
+        is full of vanlife enthusiasts who know firsthand the magic of touring
+        the world on 4 wheels.
+      </p>
+      </div>
+      <div className="about-page-cta">
+        <h2>Your destination is waiting. Your van is ready.</h2>
+        <Link className="link-button">
+          Explore our vans
+        </Link>
+      </div>
+    </div>
+  );
 }
--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -2,8 +2,7 @@ import React from "react"
 import { NavLink, Outlet } from "react-router-dom"
 
 export default function HostLayout() {
-
-  const activeStyles = {
+const activeStyles = {
     fontWeight: "bold",
     textDecoration: "underline",
     color: "#161616"
