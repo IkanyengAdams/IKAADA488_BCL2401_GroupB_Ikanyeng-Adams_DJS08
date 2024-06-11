981758d820cc3bb09d2d057862cef55242bf6923
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-06-11 09:43:37 +0200

    Remove Link import from react-router-dom in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Vans from "./Vans";
